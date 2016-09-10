b1a31501790c0cb573e883432ccbc20c816958af
Douglas Grogg <dgrogg@Douglass-Air.lan>
2016-09-09 23:22:57 -0400

    updated routes in app file

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-let koa = require('koa');
-let app = koa();
-let router = require('koa-router');
-let mount = require('koa-mount');
+var koa = require('koa');
+var app = koa();
+var router = require('koa-router');
+var mount = require('koa-mount');
+var api = require('./api/api.js');
  
-let handler = function *(next){
-    this.type = 'json';
-    this.status = 200;
-    this.body = {'Welcome': 'This is a level 2 Hello World Application!!'};
-};
+var APIv1 = new router();
+APIv1.get('/all', api.all);
+APIv1.get('/single', api.single);
  
-let APIv1 = new router();
-APIv1.get('/all', handler);
  
 app.use(mount('/v1', APIv1.middleware()));
 if (!module.parent) app.listen(3000);
-console.log('Hello World is Running on http://localhost:3000/');
+console.log('Dictapi is Running on http://localhost:3000/');
